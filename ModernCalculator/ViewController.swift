159b4053b75ded1000272ce684d8f23bb54d5f38
Cooper <coopersimson@outlook.com>
2016-03-25 17:33:42 -0700

    Initial Commit

---
+++ ModernCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ModernCalculator
+//
+//  Created by Cooper Simson on 2016-03-25.
+//  Copyright © 2016 Cooper Simson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
